345d7f833037dd9aff9ca6160d6b6b6aebfc241c
lucman007 <68017206+lucman007@users.noreply.github.com>
2023-09-07 13:17:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 3000 });
+
+wss.on('connection', (ws) => {
+    console.log('Cliente conectado');
+    ws.on('message', (message) => {
+        const messageText = message.toString();
+        ws.send(messageText);
+    });
+    ws.on('close', () => {
+        console.log('Cliente desconectado');
+    });
+});
+
+wss.on('listening', () => {
+    console.log('Servidor WebSocket en ejecución en el puerto 3000');
+});
