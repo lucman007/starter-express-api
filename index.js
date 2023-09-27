b2ae2a0537f23920af205e52c13e8e67cdeda843
lucman007 <68017206+lucman007@users.noreply.github.com>
2023-09-27 11:48:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const WebSocket = require('ws');
 const PORT = process.env.PORT || 10000;
 const wss = new WebSocket.Server({ port: PORT });
 
+const clients = new Set();
+
 wss.on('connection', (ws) => {
     console.log('Cliente conectado');
+
+    clients.add(ws);
+
     ws.on('message', (message) => {
-        const messageText = message.toString();
-        ws.send(messageText);
+        const receivedObj = JSON.parse(message);
+        const resendMessage = JSON.stringify(receivedObj)
+        clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(resendMessage);
+            }
+        });
     });
+
     ws.on('close', () => {
         console.log('Cliente desconectado');
+        clients.delete(ws);
     });
 });
 
